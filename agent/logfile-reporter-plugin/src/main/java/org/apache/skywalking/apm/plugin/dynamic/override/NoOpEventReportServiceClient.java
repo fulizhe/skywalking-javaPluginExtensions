9ba94e7027a32059ddfc6283cd7aeaac1b79d48f
fulizhe <184759758@qq.com>
2026-03-31 11:01:51 +0800

    NoOpEventReportServiceClient

--- agent/logfile-reporter-plugin/src/main/java/org/apache/skywalking/apm/plugin/dynamic/override/NoOpEventReportServiceClient.java
+++ agent/logfile-reporter-plugin/src/main/java/org/apache/skywalking/apm/plugin/dynamic/override/NoOpEventReportServiceClient.java
@@ -7,8 +7,9 @@ import org.apache.skywalking.apm.agent.core.remote.EventReportServiceClient;
 import org.apache.skywalking.apm.agent.core.remote.GRPCChannelStatus;
 
 /**
- * 本类型的作用就是向 OAP侧发送两个事件: StartingEvent, shutdownEvent. 这在我们的场景下用处不大
- * 
+ * <p> 本类型的作用就是向 OAP侧发送两个事件: StartingEvent, shutdownEvent. 这在我们的场景下用处不大
+ * <p> 注意基类所在的package, 可以说本项目要解决的问题基本都在这个package下.
+ * <p> {@link https://github.com/apache/skywalking-java/blob/b1351bcc826de086b65212c87b232524ef33a7c1/apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/remote/EventReportServiceClient.java}
  * @deprecated 暂未启用. 
  */
 @Deprecated
